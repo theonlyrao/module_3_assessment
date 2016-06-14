bc4c85b9a3c665e68989953e0dd3fb8b05e5c2f4
Ashwin Rao <theonlyrao@gmail.com>
2016-06-14 11:00:47 -0600

    Continue to work on getting service set up right

--- app/services/best_buy_service.rb
+++ app/services/best_buy_service.rb
@@ -3,7 +3,7 @@ class BestBuyService
   attr_reader :conn, :key
   
   def initialize
-    @conn = Faraday.new(:url => 'https://api.bestbuy.com/v1') do |faraday|
+    @conn = Faraday.new(:url => 'https://api.bestbuy.com') do |faraday|
       faraday.request  :url_encoded      
       faraday.response :logger           
       faraday.adapter  Faraday.default_adapter
@@ -11,12 +11,13 @@ class BestBuyService
     @key = ENV["BEST_BUY_KEY"]
   end
 
-  def self.stores_by_zip(zip_code)
+  def self.stores_by_zip(zip_code, radius=10, num_per_page=15)
     service = BestBuyService.new
     service.conn.get do |req|
-      req.url "/stores(postalCode=#{zip_code})"
+      req.url "v1/stores(area(#{zip_code},#{radius})"
       req.params["format"] = "json"
       req.params["show"] = "longName,city,distance,phone,storeType"
+      req.params["pageSize"] = num_per_page
       req.params["apiKey"] = ENV["BEST_BUY_KEY"]
     end
   end
